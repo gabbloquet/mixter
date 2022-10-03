55238992daf303b0487261a6f1cf48bfe34885ed
Gabin Bloquet <gabin.blqt@gmail.com>
2022-10-03 07:57:23 +0200

    chore: third step :tada:

--- domain/src/main/java/mixter/domain/core/subscription/Subscription.java
+++ domain/src/main/java/mixter/domain/core/subscription/Subscription.java
@@ -1,7 +1,15 @@
 package mixter.domain.core.subscription;
 
 import mixter.doc.Aggregate;
+import mixter.domain.EventPublisher;
+import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.identity.UserId;
 
 @Aggregate
 public class Subscription {
+    public static void follow(UserId follower, UserId followee, EventPublisher eventPublisher) {
+        UserFollowed userFollowed = new UserFollowed(new SubscriptionId(follower, followee));
+
+        eventPublisher.publish(userFollowed);
+    }
 }
